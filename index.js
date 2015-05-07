8b17388562862ed614a8a390f2c064cdb3f1c268
John Otander <johnotander@gmail.com>
2015-05-06 23:15:25 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function hasChildSelector(options) {
+  options = options || {};
+
+  return true;
+};
---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+var hasChildSelector = require('..');
+
+describe('has-child-selector', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(hasChildSelector(), true);
+  });
+});
