4f0a6981678cc14e76f2f6a838ce59b20b3db25a
John Otander <johnotander@gmail.com>
2015-12-15 08:01:16 -0700

    Update package, use ava

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-module.exports = function hasChildSelector(selector) {
-  if (typeof selector != 'string') {
-    throw new TypeError('has-child-selector expects a string');
+module.exports = function hasChildSelector (selector) {
+  if (typeof selector !== 'string') {
+    throw new TypeError('has-child-selector expects a string')
   }
 
-  return />/.test(selector);
-};
+  return />/.test(selector)
+}
